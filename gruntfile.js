e926495b7824403841562863348d58b667cc08f5
James Van Waza <jgacuca@gmail.com>
2015-10-30 12:55:04 -0400

    Added Grunt-Contrib-SASS to gruntfile.js

--- gruntfile.js
+++ gruntfile.js
@@ -71,6 +71,7 @@ module.exports = function(grunt) {
     });
 
     grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-compass');
     grunt.loadNpmTasks('grunt-postcss');
