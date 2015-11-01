0ffc05124d7f037752fa8c2be52b0e24b686ab1b
James Van Waza <jgacuca@gmail.com>
2015-11-01 08:50:46 -0500

    PSR-4 Format

--- nav.php
+++ nav.php
@@ -5,23 +5,16 @@
                 <h1><a href="index.php">Soccer Statistics</a></h1>
             </li>
             <!-- Remove the class "menu-icon" to get rid of menu icon. Take out "Menu" to just have icon alone -->
-            <li class="toggle-topbar menu-icon"><a href="#"><span>MENU</span></a>
-        </li>
-    </ul>
-    <section class="top-bar-section">
-        <!-- Right Nav Section -->
-        <ul class="right">
-        <li><a href="about.php" title="About Us">About Us</a>
-        </li>
-        <li><a href="faqs.php" title="Frequently Asked Questions">FAQs</a>
-        </li>
-        <li><a href="contact.php" title="Contact us">Contact Us</a>
-        </li>
-        <li><a href="index.php" title="Return to Home Page">Home Page</a>
-        </li>
-        <li><a href="index.php" title="Return to Home Page">Home Page</a>
-        </li>
-      </ul>
-</section>
-</nav>
+            <li class="toggle-topbar menu-icon"><a href="#"><span>MENU</span></a></li>
+        </ul>
+        <section class="top-bar-section">
+            <!-- Right Nav Section -->
+            <ul class="right">
+                <li><a href="about.php" title="About Us">About Us</a></li>
+                <li><a href="faqs.php" title="Frequently Asked Questions">FAQs</a></li>
+                <li><a href="contact.php" title="Contact us">Contact Us</a></li>
+                <li><a href="index.php" title="Return to Home Page">Home Page</a></li>
+            </ul>
+        </section>
+    </nav>
 <?php include 'footer.php';?>
