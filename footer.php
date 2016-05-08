d4f1ceb1df379fe67d2b0128c23ce06fe8d0df3e
JamesVanWaza <jgacuca@gmail.com>
2016-05-07 21:54:12 -0400

    Added Copyright page for footer

---
+++ footer.php
@@ -0,0 +1,6 @@
+	<footer>
+		<i class="fa fa-copyright"></i> JAMESVANWAZA 2012-<?php echo date('Y'); ?>
+	</footer>
+	<script src="bower_components/jquery/dist/jquery.min.js"></script>
+</body>
+</html>
