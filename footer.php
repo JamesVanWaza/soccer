95f4e96d0db895fff6d58209b0928c98e59088b6
JamesVanWaza <jgacuca@gmail.com>
2016-06-11 18:27:37 -0400

    Added Flexnav Minified JS

--- footer.php
+++ footer.php
@@ -2,5 +2,6 @@
 		<i class="fa fa-copyright"></i> JAMESVANWAZA 2012-<?php echo date('Y'); ?>
 	</footer>
 	<script src="bower_components/jquery/dist/jquery.min.js"></script>
+	<script src="js/flexnav.min.js"></script>
 </body>
 </html>
