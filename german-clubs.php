e895fbb14ab80150f10fabfe991db5c2068ed355
JamesVanWaza <jgacuca@gmail.com>
2016-06-11 21:22:25 -0400

    Rewriting of the page

--- german-clubs.php
+++ german-clubs.php
@@ -1,15 +1,3 @@
-<!doctype html>
-<html>
-<head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width">
-<title>German Clubs</title>
-<link href="css/foundation.css" rel="stylesheet" type="text/css">
-<link href="css/normalize.css" rel="stylesheet" type="text/css">
-<link href="css/main.css" rel="stylesheet" type="text/css">
-<script src="js/main.js"></script>
-</head>
-<?php include_once 'nav.php'; ?>
-<body>
-</body>
-</html>
+<?php include 'nav.php';
+
+include 'footer.php';
