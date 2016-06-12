7925911bf7276a47f20302792563de0ce4c286cc
JamesVanWaza <jgacuca@gmail.com>
2016-06-11 21:00:12 -0400

    Included the footer at the bottom of the page

--- view-club.php
+++ view-club.php
@@ -1,4 +1,4 @@
-<?php include "nav.php";?>
+<?php include 'nav-admin.php';?>
 <h2>These are the list of clubs</h2>
 <?php
 // This script retrieves all the records from the `soccer`.`clubs` table.
@@ -8,28 +8,29 @@ $q = "SELECT * FROM `clubs`";
 $result = @mysqli_query($dbcon, $q); // Run the query.
 
 if ($result) {
-	// If it ran OK, display the records.
-	// Table header.
-	echo '<table>
+    // If it ran OK, display the records.
+    // Table header.
+    echo '<table>
 			<tr>
 				<td><b>Club No</b></td>
 				<td><b>Name of club</b></td>
 			</tr>';
-	// Fetch and print all the records:
-	while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
-		echo '<tr>
+    // Fetch and print all the records:
+    while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+        echo '<tr>
 					<td class="text-center">' . $row['id'] . '</td>
 					<td>' . $row['noc'] . '</td>
 				</tr>';
-	}
-	echo '</table>'; // Close the table.
-	mysqli_free_result($result); // Free up the resources.
+    }
+    echo '</table>'; // Close the table.
+    mysqli_free_result($result); // Free up the resources.
 } else {
-	// If it did not run OK.
-	// Public message:
-	echo '<p class="alert-box alert round">The current users could not be retrieved. We apologize for any inconvenience.</p>';
-	// Debugging message:
-	echo '<p>' . mysqli_error($dbcon) . '<br><br />Query: ' . $q . '</p>';
+    // If it did not run OK.
+    // Public message:
+    echo '<p class="alert-box alert round">The current users could not be retrieved. We apologize for any inconvenience.</p>';
+    // Debugging message:
+    echo '<p>' . mysqli_error($dbcon) . '<br><br />Query: ' . $q . '</p>';
 } // End of if ($r) IF.
 mysqli_close($dbcon); // Close the database connection.
+include 'footer.php';
 ?>
