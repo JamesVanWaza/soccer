f3f65794846662b43fe084191dc32711d0541c53
James Van Waza <jgacuca@gmail.com>
2016-06-19 15:26:57 -0400

    Will be using nav-admin instead of admin-nav.php

--- nav-admin.php
+++ nav-admin.php
@@ -1,24 +1,11 @@
 <?php include_once 'html5req.php';?>
-<nav class="top-bar" data-topbar role="navigation" data-options="is_hover: false">
-    <ul class="title-area">
-        <li class="name">
-            <h1><a href="index.php">Soccer Statistics</a></h1>
-        </li>
-        <!-- Remove the class "menu-icon" to get rid of menu icon. Take out "Menu" to just have icon alone -->
-        <li class="toggle-topbar menu-icon"><a href="#"><span>MENU</span></a></li>
-    </ul>
-    <section class="top-bar-section">
-        <!-- Right Nav Section -->
-        <ul class="right">
-            <li><a href="view-uefaclubs.php">UEFA Stats</a></li>
-            <li><a href="view-club.php">View Clubs</a></li>
-        </ul>
-    </section>
-</nav>
+<div class="menu-button">Menu</div>
 <nav>
     <ul class="flexnav" data-breakpoint="800">
-        <li><a href="create-club.php">Create Clubs</a></li>
-        <li><a href="view-club.php">View Clubs</a></li>
+        <li><a href="about.php" title="About Us">About Us</a></li>
+        <li><a href="faqs.php" title="Frequently Asked Questions">FAQs</a></li>
+        <li><a href="contact.php" title="Contact us">Contact Us</a></li>
+        <li><a href="index.php" title="Return to Home Page">Home Page</a></li>
         <li><a href="#">Logout</a></li>
     </ul>
 </nav>
