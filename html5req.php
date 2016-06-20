5a92552b21f6f449b53673db4a6a541e4274eb8c
JamesVanWaza <jgacuca@gmail.com>
2016-06-20 11:39:15 -0400

    Added the Favicon Icon

--- html5req.php
+++ html5req.php
@@ -5,4 +5,9 @@
 <head>
     <link href="css/main.css" rel="stylesheet" type="text/css">
     <link href="node_modules/components-font-awesome/css/font-awesome.css" rel="stylesheet" type="text/css">
+    <link rel="icon" type="image/png" href="images/SoccerFavicon32.png" sizes="32x32">
+    <link rel="icon" type="image/png" href="images/SoccerFavicon64.png" sizes="64x64">
+    <link rel="icon" type="image/png" href="images/SoccerFavicon128.png" sizes="128x128">
+    <link rel="icon" type="image/png" href="images/SoccerFavicon256.png" sizes="256X256>
+    <link rel="icon" type="image/png" href="images/SoccerFavicon512.png" sizes="512x512">
 </head>
