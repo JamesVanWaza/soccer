2276f08919824bb41f5364de15d924681c104836
JamesVanWaza <jgacuca@gmail.com>
2016-08-06 14:52:57 -0400

    Moved the homepage navigation to the top of the list items

--- nav-admin.php
+++ nav-admin.php
@@ -2,10 +2,10 @@
 <div class="menu-button">Menu</div>
 <nav>
     <ul class="flexnav" data-breakpoint="800">
+       <li><a href="index.php" title="Return to Home Page">Home Page</a></li>
         <li><a href="about.php" title="About Us">About Us</a></li>
         <li><a href="faqs.php" title="Frequently Asked Questions">FAQs</a></li>
         <li><a href="contact.php" title="Contact us">Contact Us</a></li>
-        <li><a href="index.php" title="Return to Home Page">Home Page</a></li>
         <li><a href="#">Logout</a></li>
     </ul>
 </nav>
