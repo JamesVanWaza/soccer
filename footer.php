c2a5121218463a501785e7e01552d8214674b095
JamesVanWaza <jgacuca@gmail.com>
2016-08-06 15:10:50 -0400

    PSR-4 Format

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 	<footer id="footer">
 		<i class="fa fa-copyright"></i>
-		<?php
+<?php
 $fromYear = 2012;
 $thisYear = (int) date('Y');
 echo $fromYear . (($fromYear != $thisYear) ? '-' . $thisYear : '');
@@ -8,6 +8,7 @@ echo $fromYear . (($fromYear != $thisYear) ? '-' . $thisYear : '');
 	</footer>
 	<script src="node_modules/jquery/dist/jquery.min.js"></script>
 	<script src="js/flexnav.js"></script>
+	<script src="js/datatables.min.js"></script>
 	<script src="js/main.js"></script>
 </body>
 </html>
