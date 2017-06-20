ccdd589d9eaef60cd4784959483223f7d9320c72
JamesVanWaza <info@dosbranding.com>
2017-06-20 19:22:09 -0400

    Added the missing closing quotation marks
    Removed Components Font Awesome
    Using Font Awesome

--- html5req.php
+++ html5req.php
@@ -4,10 +4,10 @@
 <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 <head>
     <link href="css/main.css" rel="stylesheet" type="text/css">
-    <link href="node_modules/components-font-awesome/css/font-awesome.css" rel="stylesheet" type="text/css">
+    <link href="node_modules/font-awesome/css/font-awesome.css" rel="stylesheet" type="text/css">
     <link rel="icon" type="image/png" href="images/SoccerFavicon32.png" sizes="32x32">
     <link rel="icon" type="image/png" href="images/SoccerFavicon64.png" sizes="64x64">
     <link rel="icon" type="image/png" href="images/SoccerFavicon128.png" sizes="128x128">
-    <link rel="icon" type="image/png" href="images/SoccerFavicon256.png" sizes="256X256>
+    <link rel="icon" type="image/png" href="images/SoccerFavicon256.png" sizes="256X256">
     <link rel="icon" type="image/png" href="images/SoccerFavicon512.png" sizes="512x512">
 </head>
