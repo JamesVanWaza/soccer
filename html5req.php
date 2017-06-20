4c04ed2e4f95f7d6409b479ffb9072910eb3530c
JamesVanWaza <info@dosbranding.com>
2017-06-20 19:23:50 -0400

    Removed main.css changed it to style.css

--- html5req.php
+++ html5req.php
@@ -3,7 +3,7 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 <head>
-    <link href="css/main.css" rel="stylesheet" type="text/css">
+    <link href="css/style.css" rel="stylesheet" type="text/css">
     <link href="node_modules/font-awesome/css/font-awesome.css" rel="stylesheet" type="text/css">
     <link rel="icon" type="image/png" href="images/SoccerFavicon32.png" sizes="32x32">
     <link rel="icon" type="image/png" href="images/SoccerFavicon64.png" sizes="64x64">
