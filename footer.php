bd6aaadbe2601efcd21254e9687a1bdcdf19ddf4
JamesVanWaza <info@dosbranding.com>
2017-08-21 14:16:04 -0400

    Using Node_Modules Instead

--- footer.php
+++ footer.php
@@ -7,8 +7,8 @@ echo $fromYear . (($fromYear != $thisYear) ? '-' . $thisYear : '');
 ?> , James Van Waza
 	</footer>
 	<script src="node_modules/jquery/dist/jquery.min.js"></script>
-	<script src="js/flexnav.js"></script>
-	<script src="js/datatables.min.js"></script>
-	<script src="js/main.js"></script>
+	<script src="node_modules/jquery/dist/jquery.min.js"></script>
+	<script src="node_modules/jquery/dist/jquery.min.js"></script>
+	<script src="node_modules/jquery/dist/jquery.min.js"></script>
 </body>
 </html>
