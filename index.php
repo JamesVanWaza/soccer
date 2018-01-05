7f790bdce9cbba8fc7e98141130419a7a2300dbd
JamesVanWaza <info@dosbranding.com>
2018-01-05 18:49:05 -0500

    Added 2 Links

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php include 'html5req.php';?>
   <body>
     <h1>Hello, world!</h1>
-
+	<a href="create-club.php">Create Club</a>
+	  <br>
+	<a href="create-uefaclubs.php">Create UEFA Clubs</a>
     <script src="node_modules/jquery/dist/jquery.min.js"></script>
     <script src="node_modules/what-input/dist/what-input.js"></script>
     <script src="node_modules/foundation-sites/dist/js/foundation.min.js"></script>
