22caeeab75cb7848895e05794757ff4032b8f766
JamesVanWaza <info@dosbranding.com>
2018-04-20 20:21:33 -0400

    Removed the FAQ Link

--- nav.php
+++ nav.php
@@ -4,7 +4,6 @@
     <ul class="flexnav" data-breakpoint="800">
     	<li><a href="index.php" title="Return to Home Page">Home Page</a></li>
         <li><a href="about.php" title="About Us">About Us</a></li>
-        <li><a href="faqs.php" title="Frequently Asked Questions">FAQs</a></li>
         <li><a href="contact.php" title="Contact us">Contact Us</a></li>
         <li><a href="login.php">Login</a></li>
     </ul>
