be088dd85849bfb73c1325c73379cf0e52c39622
JamesVanWaza <info@dosbranding.com>
2018-09-21 14:16:10 -0400

    Changed Font Awesome 4 to FontAwesome5

--- html5req.php
+++ html5req.php
@@ -4,7 +4,7 @@
 <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 <head>
     <link href="css/style.css" rel="stylesheet" type="text/css">
-    <link href="node_modules/font-awesome/css/font-awesome.css" rel="stylesheet" type="text/css">
+	<script src="node_modules/@fortawesome/fontawesome-free/js/all.min.js"></script>
     <link rel="icon" type="image/png" href="images/SoccerFavicon32.png" sizes="32x32">
     <link rel="icon" type="image/png" href="images/SoccerFavicon64.png" sizes="64x64">
     <link rel="icon" type="image/png" href="images/SoccerFavicon128.png" sizes="128x128">
